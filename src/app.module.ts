fba3561a82a8aa91837a2d6294eec7d79dae5f49
mohiniprasad <mohini.prasad@stackinfinite.com>
2023-07-19 11:50:43 +0530

    Api's in nest js

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule,
+    AuthModule,
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
+})
+export class AppModule {}
